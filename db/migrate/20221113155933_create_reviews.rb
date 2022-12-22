6228fc3bdd5c3240045bbaf16d792bafc59537e2
EC2 Default User <ec2-user@ip-172-31-44-191.ap-northeast-1.compute.internal>
2022-12-22 12:47:45 +0000

    tableを変えた

--- db/migrate/20221113155933_create_reviews.rb
+++ db/migrate/20221113155933_create_reviews.rb
@@ -2,8 +2,6 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
        t.integer :user_id,             null: false, default: ""
-       t.integer :comment_id,          null: false, default: ""
-       t.integer :favorite_id,         null: false, default: ""
        t.string :music_title,          null: false, default: ""
        t.string :music_url,            null: false, default: ""
        t.string :music_artist_name,    null: false, default: ""
--- db/migrate/20221127045007_remove_comment_id_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCommentIdFromReviews < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :reviews, :comment_id, :integer
-  end
-end
